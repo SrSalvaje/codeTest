9dd59f2f1f0d81ccfe9d06fec953d81dfc66cf2c
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-06 14:59:25 +0200

    chore: initial commit

---
+++ src/arrays.test.js
@@ -0,0 +1,83 @@
+const people = [
+  {
+    name: 'Fred',
+    age: 27,
+    interests: [
+      'bowling',
+      'golf',
+    ],
+  },
+  {
+    name: 'Wilma',
+    age: 25,
+    interests: [
+      'running',
+      'cinema',
+    ],
+  },
+  {
+    name: 'Barney',
+    age: 28,
+    interests: [
+      'bowling',
+      'running',
+      'swimming',
+    ],
+  }
+];
+
+describe('getNamesOfPeopleOver', () => {
+  const getNamesOfPeopleOver = limit => {
+    // TODO: Complete this function to allow the test to pass
+  };
+
+  it('returns an array of the names of all people over the given age', () => {
+    assert.deepEqual(getNamesOfPeopleOver(26), ['Fred', 'Barney']);
+  });
+});
+
+describe('getSumOfAllAges', () => {
+  const getSumOfAllAges = () => {
+    // TODO: Complete this function to allow the test to pass
+  };
+
+  it('calculates the sum of all ages', () => {
+    assert.equal(getSumOfAllAges(), 80);
+  });
+});
+
+describe('getInterests', () => {
+  const getInterests = () => {
+    // TODO: Complete this function to allow the test to pass
+  };
+
+  it('returns a array of all interests, no duplicates, sorted', () => {
+    assert.deepEqual(getInterests(), ['bowling', 'cinema', 'golf', 'running', 'swimming']);
+  });
+});
+
+describe('getNamesAndInterests', () => {
+  const getNamesAndInterests = () => {
+    // TODO: Complete this function to allow the test to pass
+  };
+
+  it('returns an object with the names as keys, and the interests as values', () => {
+    const expected = {
+      Fred: [
+        'bowling',
+        'golf',
+      ],
+      Wilma: [
+        'running',
+        'cinema',
+      ],
+      Barney: [
+        'bowling',
+        'running',
+        'swimming',
+      ],
+    };
+
+    assert.deepEqual(getNamesAndInterests(), expected);
+  });
+});
---
+++ src/closure.test.js
@@ -0,0 +1,25 @@
+describe('multiplier', () => {
+  const multiplier = () => {
+    // TODO: Complete this function to allow the test to pass
+  };
+
+  describe('when given a number, a function is returned which when called', () => {
+    it('returns the multiplication of the two numbers', () => {
+      const times12 = multiplier(12);
+
+      assert.equal(times12(3), 36);
+    });
+  });
+});
+
+describe('counter', () => {
+  const count = () => {
+    // TODO: Complete this function to allow the test to pass
+  };
+
+  it('returns an increasing number each time it is called', () => {
+    assert.equal(count(), 0);
+    assert.equal(count(), 1);
+    assert.equal(count(), 2);
+  });
+});
